59c92caa606aeb9c93542ab86a743902632288e3
Sai Deekshith <rsaideekshith@gmail.com>
2021-07-26 07:14:07 +0530

    more oepraions added

--- addshell.sh
+++ addshell.sh
@@ -5,4 +5,10 @@ read -p "enter fourth number: " num4
 read -p "enter fifth number: " num5
 sum=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
 echo "sum is: $sum"
+echo $(( num3 + num5 ))
+echo $(( num4 - num2 ))
+echo $(( num4 / num5 ))
+echo $(( num2 % num4 ))
+echo $(( num1 * num3 ))
+
 
