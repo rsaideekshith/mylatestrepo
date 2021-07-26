6ec2c243971434d3d53b3df70442cbdbc843ec3f
Sai Deekshith <rsaideekshith@gmail.com>
2021-07-26 05:52:44 +0530

    addition is done

---
+++ addshell.sh
@@ -0,0 +1,8 @@
+read -p "enter first number: " num1
+read -p "enter second number:" num2
+read -p "enter third number: " num3
+read -p "enter fourth number: " num4
+read -p "enter fifth number: " num5
+sum=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
+echo "sum is: $sum"
+
