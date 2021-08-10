428a29459fa6da005a0db1f26bcc543440167ada
Sai Deekshith <rsaideekshith@gmail.com>
2021-08-10 11:19:51 +0530

    palindrome file is modified and exam file is deleted

--- palin.sh
+++ palin.sh
@@ -5,10 +5,18 @@ reverse=0
 while [ $number -gt 0 ]
 do
 	rem=$(($number % 10 ))
+	number=$(( number / 10 ))
 	reverse=$(( reverse * 10 + $rem ))
-	number=$(( $number/10 ))
 done
 echo " reverse=$reverse"
+if [ $reverse -eq $number ]
+then
+	echo " number is palindrome "
+else
+	echo " number is not palindrome "
+fi
+
+
 
 
 
